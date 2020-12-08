95f0a3f751ea60ff8e2354862fd0b944a4e6c451
vitor20022003 <vitorm4123@gmail.com>
2020-12-08 00:33:49 -0300

    POO heranca concluido

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using POO_Heranca.classes;
+
+namespace POO_Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Pessoa Física
+            PessoaFisica pf = new PessoaFisica();
+
+            pf.nome = "vitor";
+
+            pf.cpf = "503.406.366-29";
+
+            Console.WriteLine(pf.DarBoasVindas(pf.nome));
+
+            Console.WriteLine($"CPF Validado: {pf.ValidarCPF(pf.cpf)}");
+
+            // Pessoa Jurídica
+            PessoaJuridica pj = new PessoaJuridica();
+
+            pj.nome = "manoel";
+
+            Console.WriteLine(pj.DarBoasVindas(pj.nome));
+
+            pj.cnpj = "61.643.558/8765-65";
+
+            pj.inscricaoEstadual = "602-787-349-21";
+
+            Console.WriteLine($"CNPJ Validado: {pj.ValidarCNPJ(pj.cnpj)}");
+        }
+    }
+}
---
+++ classes/Pessoa.cs
@@ -0,0 +1,11 @@
+namespace POO_Heranca.classes
+{
+    public class Pessoa
+    {
+        public string nome;
+
+        public string DarBoasVindas(string NomeDoUsuario){
+            return "Olá "+NomeDoUsuario+"! Seja bem vindo(a) ao nosso sistema";
+        }
+    }
+}
